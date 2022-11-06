54656882ba456b69272ad1ea577a7d24182edae0
Jorge Aguinaga <54590716+Jagu7165@users.noreply.github.com>
2022-11-06 14:20:34 -0700

    navbar stuff

--- TimeTrackerV2/Angular/src/app/app.component.ts
+++ TimeTrackerV2/Angular/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  userType!: string;
+  instructor: boolean = false;
+  student: boolean = false;
+  admin: boolean = false;
+  nullType: boolean = false;
+
+
+  ngOnInit(): void {
+    let currentUser = localStorage.getItem('currentUser');
+    var userDate = currentUser ? JSON.parse(currentUser) : null;
+    this.userType = userDate.type;
+    if(this.userType === 'admin') {
+      this.admin = true;
+    }
+    else if(this.userType === 'instructor'){
+      this.instructor = true;
+    }else if(this.userType === 'student'){
+      this.student = true;
+    }else{
+      this.nullType = true;
+    }
+    console.log(this.userType);
+  }
   title = 'TimeTrackerV2';
 }
