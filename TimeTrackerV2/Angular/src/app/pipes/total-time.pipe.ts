fed245c6e8fd0fa425100e08292c1abce23c59e5
STAT3FARM <jacobvanbeekum@mail.weber.edu>
2022-11-05 22:46:48 -0600

    Minor commenting fixes on total-time.pipe.ts

--- TimeTrackerV2/Angular/src/app/pipes/total-time.pipe.ts
+++ TimeTrackerV2/Angular/src/app/pipes/total-time.pipe.ts
@@ -6,14 +6,15 @@ import {Pipe, PipeTransform} from '@angular/core';
 export class TotalTimePipe implements PipeTransform {
 
   transform(totalTime: number): string {
+    // Seconds Conversion
     if (totalTime < 60000)
       return `${(totalTime / 1000).toFixed(2)} sec`;
 
-    // Megabyte conversion
+    // Minutes Conversion
     if (totalTime >= 60000 && totalTime < 3600000)
       return `${(totalTime / 1000 / 60).toFixed(2)} min`;
 
-    // Gigabyte conversion
+    // Hours Conversion
     if (totalTime >= 3600000)
       return `${(totalTime / 1000 / 60 / 60).toFixed(2)} hr`;
 
