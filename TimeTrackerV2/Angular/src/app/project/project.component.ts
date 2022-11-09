448a6330916ffce4f4709d77703462e1d61142cd
BrendanStans21 <brendanstansfield21@gmail.com>
2022-11-08 21:47:07 -0700

    Implemented timer functionality

--- TimeTrackerV2/Angular/src/app/project/project.component.ts
+++ TimeTrackerV2/Angular/src/app/project/project.component.ts
@@ -15,9 +15,19 @@ export class ProjectComponent implements OnInit {
   private item;
   public projectName;
   public projectDescription;
+  public punches = [];
 
   date: Date = new Date();
   currDate = formatDate(this.date, 'MM/dd/yyyy', 'en-US');
+
+  seconds: any = '0' + 0;
+  minutes: any = '0' + 0;
+  hours: any = '0' + 0;
+
+  start: any;
+  isTimerRunning = false;
+  totalTime: any = "00:00:00";
+  startClickedLast = false;
 
   constructor(
     private http: HttpClient,
@@ -33,10 +43,28 @@ export class ProjectComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.loadPunches(this.punches);
+  }
+
+  loadPunches(punches: Array<string>): void {
+    this.http.get("http://localhost:8080/timeCard").subscribe((data: any) =>{ 
+    for(let i = 0; i < data.length; i++) {
+      console.log(data[i].timeIn.toString());
+      punches.push(data[i].timeIn);
+    }
+  });
   }
 
   clockIn(): void {
-    localStorage.setItem("timeIn", Date.now().toString());
+    if(!this.isTimerRunning) {
+      var nowDate = Date.now();
+      var formattedNowDate = formatDate(nowDate, 'MM/dd/yyyy HH:mm:s', 'en-US');
+
+      localStorage.setItem("timeIn", formattedNowDate);
+
+      this.startClickedLast = true;
+      this.startTimer();
+    }
 
     /*var item = localStorage.getItem('currentUser');
     
@@ -73,41 +101,48 @@ export class ProjectComponent implements OnInit {
     }*/
   }
 
-  clockOut(): void {    
-    /*var item = localStorage.getItem('currentUser');
-    
-    if (typeof item === 'string')
-    {
-      this.user = JSON.parse(item) as User
-    }
+  clockOut(): void {  
+    if(this.startClickedLast) {
+      this.startClickedLast = false;
+      this.isTimerRunning = true;  
+      this.startTimer();
 
-    if (this.user !== null )
-    {*/
-        let req = {
-          timeIn: localStorage.getItem("timeIn"), 
-          timeOut: Date.now(), /// pull date from the HTML
-          isEdited: false,
-          createdOn: Date.now(),
-          userID: 1,
-          description: "This is the Description field" /// pull description from the HTML
-        };
+      var nowDate = Date.now();
+      var formattedNowDate = formatDate(nowDate, 'MM/dd/yyyy', 'en-US');
+      var formattedTime = formatDate(nowDate, "HH:mm:s", 'en-US');
+      /*var item = localStorage.getItem('currentUser');
+    
+      if (typeof item === 'string')
+      {
+        this.user = JSON.parse(item) as User
+      }
+
+      if (this.user !== null )
+      {*/
+          let req = {
+            timeIn: localStorage.getItem("timeIn"), 
+            timeOut: formattedNowDate + " " + formattedTime, /// pull date from the HTML
+            isEdited: false,
+            createdOn: formattedNowDate,
+            userID: 1,
+            description: "This is the Description field" /// pull description from the HTML
+          };
       
 
-      /*if (req !== null)
-      {*/
-        this.http.post<any>('http://localhost:8080/clock/', req, {headers: new HttpHeaders({"Access-Control-Allow-Headers": "Content-Type"})}).subscribe({
-          next: data => {
-            this.errMsg = "";
-            console.log(req.isEdited);
-            /// populate a label to inform the user that they successfully clocked out, maybe with the time.
-          },
-          error: error => {
-            this.errMsg = error['error']['message'];
-          }
-        });
-      /*}
-    }*/
-
+        /*if (req !== null)
+        {*/
+          this.http.post<any>('http://localhost:8080/clock/', req, {headers: new  HttpHeaders({"Access-Control-Allow-Headers": "Content-Type"})}). subscribe({
+            next: data => {
+              this.errMsg = "";
+              /// populate a label to inform the user that they successfully clocked out, maybe with the time.
+            },
+            error: error => {
+              this.errMsg = error['error']['message'];
+            }
+          });
+        /*}
+      }*/
+    }
   }
 
   createGroup(): void {
@@ -127,5 +162,40 @@ export class ProjectComponent implements OnInit {
         this.errMsg = error['error']['message'];
       }
     });
+  }
+
+  startTimer(): void {
+    if(!this.isTimerRunning) {
+      this.isTimerRunning = true;
+      this.start = setInterval(() => {
+        this.seconds++;
+        this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+
+        if(this.seconds === 60) {
+          this.minutes++;
+          this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+          this.seconds = '0' + 0;
+        }
+
+        if(this.minutes === 60) {
+          this.hours++;
+          this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
+          this.minutes = '0' + 0;
+        }
+      }, 1000);
+    } else {
+      this.stopTimer();
+    }
+  }
+
+  stopTimer(): void {
+    clearInterval(this.start);
+    this.isTimerRunning = false;
+
+    this.totalTime = this.hours.toString() + ":" + this.minutes.toString() + ":" + this.seconds.toString();
+
+    this.hours = '0' + 0;
+    this.minutes = '0' + 0;
+    this.seconds = '0' + 0;
   }
 }
