2f2516f7939c2bf904325d2e64c7eb8b717bc055
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-10-16 14:53:08 -0600

    Update seed.js
    Fully implemented the default admin account.  Now only one will be created for the DB and it will be made once the table Users has been created.

--- TimeTrackerV2/NodeAPI/database/seed.js
+++ TimeTrackerV2/NodeAPI/database/seed.js
@@ -9,33 +9,30 @@ let db = new sqlite3.Database('./database/main.db', sqlite3.OPEN_READWRITE | sql
     console.log('Connected to the main database.');
   });
 
+//db.serialize();  // Serialize all following commands (not needed because we only need to serialize the Users table) https://stackoverflow.com/questions/72620312/in-node-sqlite3-how-to-wait-until-run-is-finished
+
 db.get("PRAGMA foreign_keys = ON");
 
-db.run(`CREATE TABLE IF NOT EXISTS Users(userID INTEGER PRIMARY KEY, 
-                            username TEXT NOT NULL,
-                            password TEXT NOT NULL,
-                            firstName TEXT NOT NULL,
-                            lastName TEXT NOT NULL,
-                            type TEXT NOT NULL,
-                            isActive BOOL NOT NULL,
-                            salt TEXT NOT NULL);`);
-
-// Create a super admin account with a username and password of "admin"
-/*currently doesn't work if you are making a new Database, it crashes with the error:
-undefined:0
-
-[Error: SQLITE_ERROR: no such table: Users
-Emitted 'error' event on Statement instance at:
-] {
-  errno: 1,
-  code: 'SQLITE_ERROR'
-}
-
-But once the database has been created the first time, it works.
-*/
-/*db.run(`INSERT INTO Users(username, password, firstName, lastName, type, isActive, salt)
-    SELECT "admin", "b30425b2679cfee195e07569ec1f933bac6a12759b74eccba310e75afe1313da45af4438e4be69b998d4c3bbd5ad1c990ecb6c76b8072580b49990a065ac81b6", "sudo", "admin", "admin", false, "21d20429770a51657e5b6c245c796d79"
-    WHERE NOT EXISTS(SELECT 1 FROM Users WHERE username = "admin");`);*/
+// Run the following database commands in serialized mode. I.E. only one statement can execute at a time. Other statements will wait in a queue until all the previous statements are executed.  https://www.sqlitetutorial.net/sqlite-nodejs/statements-control-flow/
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS Users(userID INTEGER PRIMARY KEY, 
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        firstName TEXT NOT NULL,
+        lastName TEXT NOT NULL,
+        type TEXT NOT NULL,
+        isActive BOOL NOT NULL,
+        salt TEXT NOT NULL);`)
+        // Create a super admin account with a username and password of "admin"
+        // It is formatted this way because the syntax order for this type of insert has to be INSERT ... SELECT instead of INSERT ... VALUES https://stackoverflow.com/a/66644198 and https://mitch.codes/sql-tip-insert-where-not-exists/
+        .run(`INSERT INTO Users(username, password, firstName, lastName, type, isActive, salt)
+            SELECT 'admin', 'b30425b2679cfee195e07569ec1f933bac6a12759b74eccba310e75afe1313da45af4438e4be69b998d4c3bbd5ad1c990ecb6c76b8072580b49990a065ac81b6', 'sudo', 'admin', 'admin', false, '21d20429770a51657e5b6c245c796d79'
+            WHERE NOT EXISTS (
+                SELECT userID
+                FROM Users
+                WHERE username = 'admin'
+            );`);
+});
 
 db.run(`CREATE TABLE IF NOT EXISTS TimeCard(timeslotID INTEGER PRIMARY KEY, 
                             timeIn TEXT NOT NULL,
