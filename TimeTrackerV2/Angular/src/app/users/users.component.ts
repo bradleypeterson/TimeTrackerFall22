f4260aac2850cca1d54f6c33f0f3202d66b56714
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-10-17 21:16:35 -0600

    Update users.component.ts
    Started to modify the logic for the Users controller so it returns a list of objects instead of a list of strings so I can select data from each object for the current user.

--- TimeTrackerV2/Angular/src/app/users/users.component.ts
+++ TimeTrackerV2/Angular/src/app/users/users.component.ts
@@ -1,37 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { concat } from 'rxjs';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.css']
 })
 
 export class UsersComponent implements OnInit {
-  public users = [];
+    public users = [];
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-  ) { }
+    constructor(
+        private http: HttpClient,
+        private router: Router,
+    ) { }
 
-  ngOnInit(): void {
-    this.loadUsers(this.users);
-  }
+    ngOnInit(): void {
+        this.loadUsers(this.users);
+    }
 
-  public pageTitle = 'TimeTrackerV2 | Users'
+    public pageTitle = 'TimeTrackerV2 | Users'
 
-  loadUsers(users: Array<String>) {
-    this.http.get("http://localhost:8080/api/Users").subscribe((data: any) => {
-      for (let i = 0; i < data.length; i++) {
-        users.push(data[i].username);
-      }
-    });
-  }
+    loadUsers(users: Array<Object>) {
+        this.http.get("http://localhost:8080/api/Users").subscribe((data: any) => {
+            for (let i = 0; i < data.length; i++) {
+                let user: Object = data[i];
 
-  navToResetPassword(tag: any, body: any) {
-    let user = JSON.parse(localStorage.getItem('currentUser') || '{}');
-    this.router.navigate(['resetpassword'], { queryParams: user })
-  }
+                /*users.push({
+                    name: user.firstName + " " + user.lastName,
+                    username: user.username
+                });*/
+            }
+        });
+    }
+
+    navToResetPassword(tag: any, body: any) {
+        let user = JSON.parse(localStorage.getItem('currentUser') || '{}');
+        this.router.navigate(['resetpassword'], { queryParams: user })
+    }
 }
