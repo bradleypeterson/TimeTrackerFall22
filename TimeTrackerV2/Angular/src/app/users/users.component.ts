99886e2c7eb0f102c9f28f01fd99932c9de5fa47
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-10-17 20:49:40 -0600

    Formatting cleanup and commenting out unnecessary code

--- TimeTrackerV2/Angular/src/app/users/users.component.ts
+++ TimeTrackerV2/Angular/src/app/users/users.component.ts
@@ -2,20 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
-
-
 @Component({
-
   selector: 'app-users',
-
   templateUrl: './users.component.html',
-
   styleUrls: ['./users.component.css']
-
 })
 
 export class UsersComponent implements OnInit {
-
   public users = [];
 
   constructor(
@@ -27,38 +20,18 @@ export class UsersComponent implements OnInit {
     this.loadUsers(this.users);
   }
 
-
-
   public pageTitle = 'TimeTrackerV2 | Users'
 
-
-
-
   loadUsers(users: Array<String>) {
-
     this.http.get("http://localhost:8080/api/Users").subscribe((data: any) => {
-
       for (let i = 0; i < data.length; i++) {
-
         users.push(data[i].username);
-
       }
-
     });
-
   }
-
-
 
   navToResetPassword(tag: any, body: any) {
-
     let user = JSON.parse(localStorage.getItem('currentUser') || '{}');
-
     this.router.navigate(['resetpassword'], { queryParams: user })
-
   }
-
-
-
 }
-
