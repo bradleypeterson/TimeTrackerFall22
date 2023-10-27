d6b5caca080bc5612b4421629bb48a0be1fa3b21
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-10-26 21:10:05 -0600

    Update DBConnection.js
    Added link for the source for the foreign key enforcement.

--- TimeTrackerV2/NodeAPI/database/DBConnection.js
+++ TimeTrackerV2/NodeAPI/database/DBConnection.js
@@ -9,7 +9,7 @@ const ConnectToDB = () => {
         }
     });
 
-    db.get("PRAGMA foreign_keys = ON");  // Turn on foreign keys for the DB connection
+    db.get("PRAGMA foreign_keys = ON");  // Turn on foreign keys for the DB connection https://stackoverflow.com/a/53085206
 
     return db;  // Return the connection to the calling function
 };
