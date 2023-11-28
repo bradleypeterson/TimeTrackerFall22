24d11bc29220e0d1c6d976da2c516d31ae7d1c2f
braxtonwright <33258249+braxtonwright@users.noreply.github.com>
2023-11-27 17:47:42 -0700

    Logic for selecting projects to assign evals implmented
    Now when you select a project from the "Projects" list, it will update some backend variables to keep track of what projects have been selected and what should be sent to the server.
    As of right now, it simply prints out the information to the console.  That portion of the logic still needs to be implmented.

--- TimeTrackerV2/Angular/src/app/assign-evals/assign-evals.component.ts
+++ TimeTrackerV2/Angular/src/app/assign-evals/assign-evals.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+interface ProjectSelection {
+  id: number;
+  name: string;
+  isSelected: boolean;
+}
+
 @Component({
   selector: 'app-assign-evals',
   templateUrl: './assign-evals.component.html',
@@ -14,7 +20,8 @@ export class AssignEvalsComponent implements OnInit {
   public currentUser: any;
 
   courseID!: number;  // The '!' at the end of "CourseID" is the "non-null assertion operator", this tell the TypeScript compiler that a value is not null or undefined, even if its type suggests that it might be
-  courseProjects: any;
+  selectAllProjects: boolean = false;
+  courseProjects: ProjectSelection[] = [];
   evalTemplates: any;
 
   constructor(
@@ -39,8 +46,18 @@ export class AssignEvalsComponent implements OnInit {
   }
 
   LoadCourseProjects() {
-    this.http.get(`https://localhost:8080/api/Projects/${this.courseID}`, { headers: new HttpHeaders({ "Access-Control-Allow-Headers": "Content-Type" }) }).subscribe({ next: data => {
-      this.courseProjects = data;
+    this.http.get<any>(`https://localhost:8080/api/Projects/${this.courseID}`, { headers: new HttpHeaders({ "Access-Control-Allow-Headers": "Content-Type" }) }).subscribe({
+      next: data => {
+        data.forEach((project: any) => {
+          // console.log("Processing the data:\n" + JSON.stringify(entry));
+          let toBeAdded: ProjectSelection = {
+            id: project.projectID,
+            name: project.projectName,
+            isSelected: false
+          };
+
+          this.courseProjects.push(toBeAdded);
+        });
       },
       error: err => {
         alert(`${err.error.message}`);
@@ -49,12 +66,34 @@ export class AssignEvalsComponent implements OnInit {
   }
 
   LoadEvalTemplates() {
-    this.http.get(`https://localhost:8080/api/templates`, { headers: new HttpHeaders({ "Access-Control-Allow-Headers": "Content-Type" }) }).subscribe({ next: data => {
-      this.evalTemplates = data;
+    this.http.get(`https://localhost:8080/api/templates`, { headers: new HttpHeaders({ "Access-Control-Allow-Headers": "Content-Type" }) }).subscribe({
+      next: data => {
+        this.evalTemplates = data;
       },
       error: err => {
         alert(`${err.error.message}`);
       }
     });
+  }
+
+  // The original idea for both the ToggleSelectAll() and OptionClicked() functions was found here and they were modified to fit my needs https://stackblitz.com/edit/angular-material-select-all-checkbox?file=src%2Fapp%2Fapp.component.ts,src%2Fapp%2Fapp.component.html
+  ToggleSelectAll(eventTarget: any) {
+    this.courseProjects.forEach((project: ProjectSelection) => {
+        project.isSelected = eventTarget.checked;
+    })
+  }
+
+  OptionClicked() {
+    let newSelectAllProjects = true;
+    // Go through each project in the course and see if all the projects have been selected.
+    this.courseProjects.forEach((project: ProjectSelection) => {
+        newSelectAllProjects = newSelectAllProjects && project.isSelected;
+    });
+    // Now assign newSelectAllProjects to selectAllProjects to signify if all the projects have been selected or not
+    this.selectAllProjects = newSelectAllProjects;
+  }
+
+  AssignEvals() {
+    console.log("API route to assign eval to the selected projects\n\"Select All\" Selected:", this.selectAllProjects, "\ncourseProjects state:", this.courseProjects);  // For debugging to make sure that the ngModel is working
   }
 }
